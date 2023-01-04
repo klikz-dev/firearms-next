7da4d775dea8f5809cf950ca5c1b2028e10544df
Murrell Johnson <dev@klikz.us>
2023-01-04 02:53:41 -0500

    feat: routing

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,19 +1,30 @@
 import { client } from '@/lib/apollo'
-import GET_PAGE_QUERY from '@/const/schema/getPage.graphql'
-import GET_PAGE_SLUGS_QUERY from '@/const/schema/getPageSlugs.graphql'
+import GET_POST_QUERY from '@/const/schema/getPost.graphql'
+import GET_POST_SLUGS_QUERY from '@/const/schema/getPostSlugs.graphql'
 import Layout from '@/components/common/Layout'
-import PageContent from '@/components/organisms/PageContent'
 import { useRouter } from 'next/router'
 import Container from '@/components/atoms/Container'
 import Loading from '@/components/atoms/Loading'
 import HTMLReactParser from 'html-react-parser'
+import PostContent from '@/components/organisms/PostContent'
 import Sidebar from '@/components/organisms/Sidebar'
-import Hero from '@/components/organisms/PageContent/Hero'
-import TOCNav from '@/components/organisms/TOCNav'
-import convertToSlug from '@/functions/convertToSlug'
+import GradientBorder from '@/components/atoms/GradientBorder'
+import PostMeta from '@/components/molecules/PostMeta'
+import Link from '@/components/atoms/Link'
+import Image from '@/components/atoms/Image'
+import HTMLContent from '@/components/atoms/HTMLContent'
 
-export default function Page({ pageData }) {
-  const { layout, hero, content } = pageData?.page?.pageContent ?? {}
+export default function Post({ postData }) {
+  const {
+    title,
+    slug,
+    seo,
+    author,
+    content,
+    date,
+    featuredImage,
+    postContent,
+  } = postData?.post ?? {}
 
   const router = useRouter()
   if (router.isFallback) {
@@ -26,57 +37,62 @@ export default function Page({ pageData }) {
     )
   }
 
-  const toc_items = content
-    ?.filter(
-      (item) => item.__typename === 'Page_Pagecontent_Content_CategorySection'
-    )
-    ?.map((item) => {
-      return { id: convertToSlug(item.title), label: item.title }
-    })
-
   return (
-    <Layout seo={HTMLReactParser(pageData?.page?.seo?.fullHead)}>
-      <Hero {...hero} />
+    <Layout seo={HTMLReactParser(seo?.fullHead)}>
+      <Container className={'py-20 grid grid-cols-3 gap-12'}>
+        <div className={'col-span-2'}>
+          <h1>{title}</h1>
 
-      {layout === 'full' && <PageContent content={content} />}
+          <GradientBorder height={2} className={'w-96 mt-4 mb-8'} />
 
-      {layout === 'sidebar' && (
-        <div className={'max-w-7xl mx-auto grid grid-cols-3 gap-16'}>
-          <div className={'col-span-2'}>
-            <PageContent content={content} />
+          <PostMeta title={title} slug={slug} author={author} date={date} />
+
+          <div className={'mt-8 mb-8 p-4 bg-zinc-200 border-l border-zinc-700'}>
+            <p className='text-sm'>
+              {
+                'Products are selected by our editors. We may earn a commission on purchases from a link. '
+              }
+              <Link
+                href={'/'}
+                className={'text-red-600 font-sans hover:underline'}
+              >
+                {'How we select gear.'}
+              </Link>
+            </p>
           </div>
-          <div className={'col-span-1 pt-20'}>
-            <Sidebar />
-          </div>
+
+          <Image
+            src={featuredImage?.node?.sourceUrl}
+            alt={featuredImage?.node?.alt}
+            width={featuredImage?.node?.mediaDetails?.width}
+            height={featuredImage?.node?.mediaDetails?.height}
+          />
+
+          <HTMLContent className={'py-8'}>{content}</HTMLContent>
+
+          <PostContent contents={postContent?.contents} />
         </div>
-      )}
 
-      {layout === 'toc' && (
-        <div className={'max-w-7xl mx-auto grid grid-cols-3 gap-16'}>
-          <div className={'col-span-2'}>
-            <PageContent content={content} />
-          </div>
-          <div className={'col-span-1 pt-20'}>
-            {toc_items?.length > 0 && <TOCNav toc_items={toc_items} />}
-          </div>
+        <div className={'col-span-1'}>
+          <Sidebar />
         </div>
-      )}
+      </Container>
     </Layout>
   )
 }
 
 export async function getStaticProps({ params }) {
   /**
-   * Page Content
+   * Post Content
    */
-  const { data: pageData, pageError } = await client.query({
-    query: GET_PAGE_QUERY,
+  const { data: postData, postError } = await client.query({
+    query: GET_POST_QUERY,
     variables: {
       slug: params.slug,
     },
   })
 
-  if (pageError) {
+  if (postError) {
     return {
       notFound: true,
     }
@@ -84,7 +100,7 @@ export async function getStaticProps({ params }) {
 
   return {
     props: {
-      pageData,
+      postData,
     },
     revalidate: 30,
   }
@@ -92,14 +108,14 @@ export async function getStaticProps({ params }) {
 
 export async function getStaticPaths() {
   const { data } = await client.query({
-    query: GET_PAGE_SLUGS_QUERY,
+    query: GET_POST_SLUGS_QUERY,
     variables: {
       first: 100,
     },
   })
 
   return {
-    paths: data.pages.nodes.map((node) => ({
+    paths: data.posts.nodes.map((node) => ({
       params: node,
     })),
     fallback: true,
---
+++ pages/guide-to-all-things-ar.js
@@ -0,0 +1,90 @@
+import { client } from '@/lib/apollo'
+import GET_PAGE_QUERY from '@/const/schema/getPage.graphql'
+import Layout from '@/components/common/Layout'
+import PageContent from '@/components/organisms/PageContent'
+import { useRouter } from 'next/router'
+import Container from '@/components/atoms/Container'
+import Loading from '@/components/atoms/Loading'
+import HTMLReactParser from 'html-react-parser'
+import Sidebar from '@/components/organisms/Sidebar'
+import Hero from '@/components/organisms/PageContent/Hero'
+import TOCNav from '@/components/organisms/TOCNav'
+import convertToSlug from '@/functions/convertToSlug'
+
+export default function Page({ pageData }) {
+  const { layout, hero, content } = pageData?.page?.pageContent ?? {}
+
+  const router = useRouter()
+  if (router.isFallback) {
+    return (
+      <Layout>
+        <Container>
+          <Loading />
+        </Container>
+      </Layout>
+    )
+  }
+
+  const toc_items = content
+    ?.filter(
+      (item) => item.__typename === 'Page_Pagecontent_Content_CategorySection'
+    )
+    ?.map((item) => {
+      return { id: convertToSlug(item.title), label: item.title }
+    })
+
+  return (
+    <Layout seo={HTMLReactParser(pageData?.page?.seo?.fullHead)}>
+      <Hero {...hero} />
+
+      {layout === 'full' && <PageContent content={content} />}
+
+      {layout === 'sidebar' && (
+        <div className={'max-w-7xl mx-auto grid grid-cols-3 gap-16'}>
+          <div className={'col-span-2'}>
+            <PageContent content={content} />
+          </div>
+          <div className={'col-span-1 pt-20'}>
+            <Sidebar />
+          </div>
+        </div>
+      )}
+
+      {layout === 'toc' && (
+        <div className={'max-w-7xl mx-auto grid grid-cols-3 gap-16'}>
+          <div className={'col-span-2'}>
+            <PageContent content={content} />
+          </div>
+          <div className={'col-span-1 pt-20'}>
+            {toc_items?.length > 0 && <TOCNav toc_items={toc_items} />}
+          </div>
+        </div>
+      )}
+    </Layout>
+  )
+}
+
+export async function getStaticProps() {
+  /**
+   * Page Content
+   */
+  const { data: pageData, pageError } = await client.query({
+    query: GET_PAGE_QUERY,
+    variables: {
+      slug: 'guide-to-all-things-ar',
+    },
+  })
+
+  if (pageError) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      pageData,
+    },
+    revalidate: 30,
+  }
+}

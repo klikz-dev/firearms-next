6b9b2a35e2c70342953f89a57e738d604d6ab4e7
Murrell Johnson <dev@klikz.us>
2023-12-29 11:18:49 -0500

    chore: refactor scripts

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,24 @@
-/* eslint-disable @next/next/no-sync-scripts */
-/* eslint-disable @next/next/next-script-for-ga */
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PZ6J7G7');`,
-          }}
-        />
-
-        <script src='https://www.google.com/recaptcha/api.js' async defer />
+        <Script id='google-analytics'>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PZ6J7G7');
+        `}
+        </Script>
 
         <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
       </Head>
+
       <body>
-        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src='https://www.googletagmanager.com/ns.html?id=GTM-PZ6J7G7'
@@ -30,8 +27,9 @@ export default function Document() {
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
-        {/* End Google Tag Manager (noscript) */}
+
         <Main />
+
         <NextScript />
       </body>
     </Html>
